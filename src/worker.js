0310c40cd75375d10869d293c884a22c2be2a678
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-02 16:39:05 -0300

    Rendering in browser function

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,6 @@
 const {v1} = require('uuid')
 const querystring = require('querystring')
+const {join} = require('path')
 
 const puppeteer = require('puppeteer')
 
@@ -20,12 +21,37 @@ function createQueryString(data) {
     )
     return qs
 }
-async function main(message) {
+
+async function render({finalURI,name }) {
+    const output = join(__dirname, `./../output/${name}-${v1()}.pdf`)
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    const page = await browser.newPage()
+    await page.goto(finalURI, {waitUntil: 'networkidle2'})
+
+    await page.pdf({
+        path: output,
+        format: 'A4',
+        landscape: true,
+        printBackground: true
+    })
+
+    await browser.close()
+}
+
+async function main(message) { 
     const pid = process.pid
     console.log(`${pid} got a message`, message.name)
     const qs = createQueryString(message)
     const finalURI = `${BASE_URL}?${qs}`
-    console.log(finalURI)
+
+    try {
+        await render({finalURI, name: message.name})
+        process.send(`${pid} has finished!`)
+    } catch (error) {
+        process.send(`${pid} has broken! ${error.stack}`)
+    }
 }
 
 process.once("message", main)
